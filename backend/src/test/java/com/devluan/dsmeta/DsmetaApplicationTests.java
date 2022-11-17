7f3474e4a9beb19c0cfce5d462a5a89bc36adb13
Luan Lima <luan_lima2011@hotmail.com>
2022-11-17 10:59:19 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devluan/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluan.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
